d062766262d4c71f87ab09b95460877dc8918049
Andrei <dang3rouz@ya.ru>
2021-04-09 15:12:44 +0500

    test

--- keystone.ts
+++ keystone.ts
@@ -45,7 +45,7 @@ export default withAuth(
   config({
     server: {
       cors: {
-        origin: [process.env.FRONTEND_URL],
+        origin: true,//[process.env.FRONTEND_URL],
         credentials: true,
       },
     },
